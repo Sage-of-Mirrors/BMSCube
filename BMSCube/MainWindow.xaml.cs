994d406d32ce3a48da445527e43ee3a0fbdfac4e
Sage_of_Mirrors <empressglatia@gmail.com>
2016-12-13 22:32:53 -0600

    Initial commit

---
+++ BMSCube/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using BMSCube.src.ViewModel;
+using System;
+using System.Windows;
+
+namespace BMSCube
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BaseMainWindowViewModel m_viewModel;
+
+        public MainWindow()
+        {
+            m_viewModel = new BaseMainWindowViewModel();
+            DataContext = m_viewModel;
+            InitializeComponent();
+        }
+    }
+}
